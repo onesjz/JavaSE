62e970c83519f995c0f063a1f04b09c9c64662fd
oneso <oneso.null@icloud.com>
2019-10-15 11:37:06 +0300

    hw02-collections

---
+++ HW02-Collections/src/main/java/com/oneso/MainClass.java
@@ -0,0 +1,40 @@
+package com.oneso;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MainClass {
+    public static void main(String[] args) {
+        List<Integer> test1 = new DIYarrayList<>();
+        List<Integer> test2 = new DIYarrayList<>();
+
+        // ---1--- //
+        Collections.addAll(test1, 1, 2, 3, 4);
+        printArray(test1);
+        System.out.println("---");
+        test1.clear();
+
+        // ---2--- //
+        for(int i = 0; i < 30; i++) {
+            test1.add(i);
+            test2.add(i + 100);
+        }
+
+        Collections.copy(test2, test1);
+        printArray(test1);
+        printArray(test2);
+        System.out.println("---");
+
+        // ---3--- //
+        Collections.sort(test1, (o1, o2) -> o2.compareTo(o1));
+        printArray(test1);
+    }
+
+    private static void printArray(Collection<?> collection) {
+        for(Object temp : collection) {
+            System.out.printf("%s ", temp);
+        }
+        System.out.println();
+    }
+}
