74ddcf38d3c886fb63f1eacd4d3cb508893e8b7d
oneso <oneso.null@icloud.com>
2019-10-23 16:39:16 +0300

    fix hw03-annotation

--- HW03-Annotations/src/main/java/com/oneso/RunnerTests.java
+++ HW03-Annotations/src/main/java/com/oneso/RunnerTests.java
@@ -21,16 +21,13 @@ public class RunnerTests {
         for(Class<?> aClass : classes) {
             Map<String, Method> methodMap = getMapMethods(aClass.getDeclaredMethods());
 
-            if(methodMap.containsKey(BEFORE_ALL))
-                if(!callMethod(methodMap.get(BEFORE_ALL), null, false)) {
-                    callMethod(methodMap.get(AFTER_ALL), null, false);
-                    throw new RuntimeException("Не удалось подготовить данные для тестов");
-                }
+            if(!methodMap.containsKey(BEFORE_ALL) || callMethod(methodMap.get(BEFORE_ALL), null, false)) {
+                callMethods(aClass.getDeclaredMethods(), methodMap, aClass);
+            }
 
-            callMethods(aClass.getDeclaredMethods(), methodMap, aClass);
-
-            if(methodMap.containsKey(AFTER_ALL))
+            if(methodMap.containsKey(AFTER_ALL)) {
                 callMethod(methodMap.get(AFTER_ALL), null, false);
+            }
         }
 
         System.out.printf("Общее количество тестов: %d%n", failed.size() + correct.size());
@@ -47,30 +44,35 @@ public class RunnerTests {
 
         for (Method method : methods) {
             if (method.isAnnotationPresent(BeforeAll.class)) {
-                if(out.containsKey(BEFORE_ALL))
+                if(out.containsKey(BEFORE_ALL)) {
                     throw new IllegalArgumentException("Не допустимо создавать более одного метода BeforeAll");
+                }
                 out.put(BEFORE_ALL, method);
             }
             if (method.isAnnotationPresent(AfterAll.class)) {
-                if(out.containsKey(AFTER_ALL))
+                if(out.containsKey(AFTER_ALL)) {
                     throw new IllegalArgumentException("Не допустимо создавать более одного метода AfterAll");
+                }
                 out.put(AFTER_ALL, method);
             }
             if (method.isAnnotationPresent(Before.class)) {
-                if(out.containsKey(BEFORE))
+                if(out.containsKey(BEFORE)) {
                     throw new IllegalArgumentException("Не допустимо создавать более одного метода Before");
+                }
                 out.put(BEFORE, method);
             }
             if (method.isAnnotationPresent(After.class)) {
-                if(out.containsKey(AFTER))
+                if(out.containsKey(AFTER)) {
                     throw new IllegalArgumentException("Не допустимо создавать более одного метода After");
+                }
                 out.put(AFTER, method);
             }
         }
 
         if(out.containsKey(BEFORE_ALL)) {
-            if(!out.containsKey(AFTER_ALL))
+            if(!out.containsKey(AFTER_ALL)) {
                 throw new RuntimeException("Необходимо создать метод с аннотацией @AfterAll");
+            }
         }
 
         return out;
@@ -83,19 +85,16 @@ public class RunnerTests {
             if(instance.isPresent()) {
 
                 if(method.isAnnotationPresent(Test.class)) {
-                    if(beforeAfter.containsKey(BEFORE))
-                        if(!callMethod(beforeAfter.get(BEFORE), instance.get(), false)) {
-                            callMethod(beforeAfter.get(AFTER), instance.get(), false);
-                            System.out.println("Не получилось подготовить данные, пропускаем " + method.getName());
-                            continue;
+                    if(!beforeAfter.containsKey(BEFORE) || callMethod(beforeAfter.get(BEFORE), instance.get(), false)) {
+                        if(method.isAnnotationPresent(Description.class)) {
+                            System.out.println(method.getDeclaredAnnotation(Description.class).value());
                         }
+                        callMethod(method, instance.get(), true);
+                    }
 
-                    if(method.isAnnotationPresent(Description.class))
-                        System.out.println(method.getDeclaredAnnotation(Description.class).value());
-                    callMethod(method, instance.get(), true);
-
-                    if(beforeAfter.containsKey(AFTER))
+                    if(beforeAfter.containsKey(AFTER)) {
                         callMethod(beforeAfter.get(AFTER), instance.get(), false);
+                    }
                 }
             }
         }
@@ -104,13 +103,15 @@ public class RunnerTests {
     private boolean callMethod(Method method, Object instance, boolean test) {
         try {
             method.invoke(instance);
-            if(test)
+            if(test) {
                 correct.add(method.getName());
+            }
             return true;
         } catch (IllegalAccessException | InvocationTargetException e) {
             System.out.println("Не удалось выполнить метод: " + method.getName());
-            if(test)
+            if(test) {
                 failed.add(method.getName());
+            }
             return false;
         }
     }
